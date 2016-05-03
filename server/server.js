167b6d859df516686e70bbd2014de806f01a59f2
teroratsu <contact.halotw@gmail.com>
2016-05-03 21:11:51 +0200

    add new connection handler on server side

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var path = require('path');
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../client', 'index.html'));
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  io.emit('update:connect','connecté');
 });
 
 http.listen(8080, function(){
