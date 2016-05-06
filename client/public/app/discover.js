4e4bf5ff638451862c6541b351d4a732834db0c0
eresis <eresis@L-CNU2469CTJ.dom1.vinci-energies.net>
2016-05-06 16:45:53 +0200

    Feature : Templating using Nunjucks

--- client/public/app/discover.js
+++ client/public/app/discover.js
@@ -1,5 +1,5 @@
 var sio = io.connect('http://localhost:8080');
 
 sio.on('update:connect', function(msg) {
-    console.log(msg)
-})
+    console.log(msg);
+});
--- server/server.js
+++ server/server.js
@@ -1,20 +1,42 @@
+/* Require et dépendences */
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var path = require('path');
 var io = require('socket.io')(http);
+var nunjucks = require('nunjucks');
 
-app.use(express.static(path.join(__dirname, '../client')));
+/* Globales et config*/
+var client_path = path.resolve(__dirname, '../client'); //adresse du dossier client
+var lobbies = [{id: 'ABCD'},{id: 'EFGH'},{id: 'IJKL'}]; //données test
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+//configuration du moteur de template
+nunjucks.configure(['views',client_path],{
+	autoescape : true,
+	express : app,
+  noCache  : true   //pour le développement. Force la recompilation du template à chaque apelle
 });
 
+/* Routing */
+app.use(express.static(client_path + '/public')); //
+app.get('/', ctrlIndex);
+
+/* Controller */
+//Renvoie la page d'accueil
+function ctrlIndex(req, res) {
+  res.render('index.html',{
+    lobbies: lobbies,
+    page: {url: req.protocol + '://' + req.get('host') + req.originalUrl}
+  });
+}
+
+/* Socket.io listeners */
 io.on('connection', function(socket){
   console.log('a user connected');
   io.emit('update:connect','connecté');
 });
 
+//Démarrage du server
 http.listen(8080, function(){
   console.log('listening on *:8080');
 });
