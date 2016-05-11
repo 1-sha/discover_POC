a5a74ab873f78707e1fd84fc8fa96adaf19fa01f
teroratsu <contact.halotw@gmail.com>
2016-05-11 21:10:16 +0200

    Merge from labo

--- server/server.js
+++ server/server.js
@@ -1,27 +1,49 @@
-/* Require et dépendences */
+/******* Require et dépendences *******/
+
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var path = require('path');
 var io = require('socket.io')(http);
 var nunjucks = require('nunjucks');
+var storage = require('node-persist');
 
-/* Globales et config*/
+/**************************************/
+/********* Globales et config *********/
+
 var client_path = path.resolve(__dirname, '../client'); //adresse du dossier client
-var lobbies = [{id: 'ABCD'},{id: 'EFGH'},{id: 'IJKL'}]; //données test
 
-//configuration du moteur de template
+var userStorage = storage.create();
+
+var lobbies = [{id: 'ABCD'},{id: 'EFGH'},{id: 'IJKL'}]; //données test
+var client = {
+  id : "5664615437832391721231523118675217" //données test
+};
+
+// Configuration Nunjucks
 nunjucks.configure(['views',client_path],{
 	autoescape : true,
 	express : app,
   noCache  : true   //pour le développement. Force la recompilation du template à chaque apelle
 });
 
-/* Routing */
+// Configuration du storage
+userStorage.initSync({
+  dir : __dirname + '/data/user'
+});
+userStorage.setItem('USER_' + client.id, client);
+//console.log(userStorage.getItem('USER_' + client.id).id);
+
+
+/**************************************/
+/************** Routing ***************/
+
 app.use(express.static(client_path + '/public')); //
 app.get('/', ctrlIndex);
 
-/* Controller */
+/**************************************/
+/************* Controller *************/
+
 //Renvoie la page d'accueil
 function ctrlIndex(req, res) {
   res.render('index.html',{
@@ -30,13 +52,19 @@ function ctrlIndex(req, res) {
   });
 }
 
-/* Socket.io listeners */
+/**************************************/
+/******** Socket.io listeners *********/
+
 io.on('connection', function(socket){
   console.log('a user connected');
   io.emit('update:connect','connecté');
 });
 
-//Démarrage du server
+/**************************************/
+/************** SERVER ****************/
+
 http.listen(8080, function(){
   console.log('listening on *:8080');
 });
+
+/**************************************/
